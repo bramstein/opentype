0248aa025227be5e480bcfd17ab32e3fcab74ea4
Bram Stein <stein@adobe.com>
2013-09-08 15:01:28 +0200

    Simplified cmap output to only include the character code to glyph id mapping and the glyph id to character code mapping.

--- src/tables/cmap.js
+++ src/tables/cmap.js
@@ -12,25 +12,23 @@ goog.scope(function () {
 
   tables.cmap = function (dataView, font) {
     var table = new Buffer(dataView),
-        data = {
-          'tables': {}
-        };
+        data = {};
 
-    data['header'] = table.read(util.struct({
-      'version': Type.USHORT,
-      'numTables': Type.USHORT
+    var header = table.read(util.struct({
+      version: Type.USHORT,
+      numTables: Type.USHORT
     }));
 
-    data['index'] = table.readArray(util.struct({
+    var index = table.readArray(util.struct({
       platformID: Type.USHORT,
       encodingID: Type.USHORT,
       offset: Type.ULONG
-    }), data['header']['numTables']);
+    }), header.numTables);
 
     data['charCode'] = {};
     data['glyph'] = [];
 
-    data['index'].forEach(function (subTable) {
+    index.forEach(function (subTable) {
       var subData = {};
 
       table.goto(subTable.offset);
@@ -41,46 +39,46 @@ goog.scope(function () {
       // only supports 0, 4, 12, 13, and (14).
       if (format === 0) {
         subData = table.read(util.struct({
-          'format': Type.USHORT,
-          'length': Type.USHORT,
-          'language': Type.USHORT
+          format: Type.USHORT,
+          length: Type.USHORT,
+          language: Type.USHORT
         }));
 
-        subData['glyphIdArray'] = table.readArray(Type.BYTE, 256);
+        var glyphIdArray = table.readArray(Type.BYTE, 256);
 
-        subData['glyphIdArray'].forEach(function (id) {
+        glyphIdArray.forEach(function (id) {
           data['charCode'][id] = id;
           data['glyph'][id] = id;
         });
       } else if (format === 4) {
         subData = table.read(util.struct({
-          'format': Type.USHORT,
-          'length': Type.USHORT,
-          'language': Type.USHORT,
-          'segCountX2': Type.USHORT,
-          'searchRange': Type.USHORT,
-          'entrySelector': Type.USHORT,
-          'rangeShift': Type.USHORT
+          format: Type.USHORT,
+          length: Type.USHORT,
+          language: Type.USHORT,
+          segCountX2: Type.USHORT,
+          searchRange: Type.USHORT,
+          entrySelector: Type.USHORT,
+          rangeShift: Type.USHORT
         }));
 
-        var segCount = subData['segCountX2'] / 2;
+        var segCount = subData.segCountX2 / 2;
 
-        subData['endCount'] = table.readArray(Type.USHORT, segCount);
-        subData['reservedPad'] = table.read(Type.USHORT);
-        subData['startCount'] = table.readArray(Type.USHORT, segCount);
-        subData['idDelta'] = table.readArray(Type.SHORT, segCount);
+        var endCount = table.readArray(Type.USHORT, segCount);
+        var reservedPad = table.read(Type.USHORT);
+        var startCount = table.readArray(Type.USHORT, segCount);
+        var idDelta = table.readArray(Type.SHORT, segCount);
 
         var idRangeTableOffset = table.byteOffset;
 
-        subData['idRangeOffset'] = table.readArray(Type.USHORT, segCount);
-        subData['glyphIdArray'] = [];
+        var idRangeOffset = table.readArray(Type.USHORT, segCount);
+        var glyphIdArray = [];
 
         for (var i = 0; i < segCount; i += 1) {
-          var start = subData['startCount'][i];
-          var end = subData['endCount'][i];
-          var delta = subData['idDelta'][i];
-          var rangeOffset = subData['idRangeOffset'][i];
-          var idRangeOffset = idRangeTableOffset + (i * Type.USHORT.sizeof);
+          var start = startCount[i];
+          var end = endCount[i];
+          var delta = idDelta[i];
+          var rangeOffset = idRangeOffset[i];
+          var offset = idRangeTableOffset + (i * Type.USHORT.sizeof);
 
           for (var j = start; j < end; j += 1) {
             var charCode = j,
@@ -89,7 +87,7 @@ goog.scope(function () {
             if (rangeOffset === 0) {
               id = (charCode + delta) % 65536;
             } else {
-              id = (table.read(Type.USHORT, idRangeOffset + rangeOffset + (charCode - start) * Type.USHORT.sizeof) + delta) % 65536;
+              id = (table.read(Type.USHORT, offset + rangeOffset + (charCode - start) * Type.USHORT.sizeof) + delta) % 65536;
             }
 
             data['charCode'][charCode] = id;
@@ -98,30 +96,30 @@ goog.scope(function () {
         }
       } else if (format === 12 ||format === 13) {
         subData = table.read(util.struct({
-          'format': Type.USHORT,
-          'reserved': Type.USHORT,
-          'length': Type.USHORT,
-          'nGroups': Type.USHORT
+          format: Type.USHORT,
+          reserved: Type.USHORT,
+          length: Type.USHORT,
+          nGroups: Type.USHORT
         }));
 
-        subData['groups'] = table.readArray(util.struct({
-          'startCharCode': Type.ULONG,
-          'endCharCode': Type.ULONG,
-          'glyphID': Type.ULONG
-        }), subData['nGroups']);
+        var groups = table.readArray(util.struct({
+          startCharCode: Type.ULONG,
+          endCharCode: Type.ULONG,
+          glyphID: Type.ULONG
+        }), subData.nGroups);
 
-        for (var i = 0; i < subData['nGroups']; i += 1) {
-          var start = subData['groups'][i]['startCharCode'];
-          var end = subData['groups'][i]['endCharCode'];
+        for (var i = 0; i < subData.nGroups; i += 1) {
+          var start = groups[i].startCharCode;
+          var end = groups[i].endCharCode;
 
           for (var j = start; j < end; j += 1) {
             var charCode = j,
                 id = null;
 
             if (format === 12) {
-              id = subData['groups'][i]['glyphId'] + j;
+              id = groups[i].glyphID + j;
             } else if (format === 13) {
-              id = subData['groups'][i]['glyphId'];
+              id = groups[i].glyphID;
             }
 
             data['charCode'][charCode] = id;
@@ -130,8 +128,6 @@ goog.scope(function () {
         }
       }
       // TODO: Add format 14
-
-      data['tables'][format] = subData;
     });
 
     return data;
