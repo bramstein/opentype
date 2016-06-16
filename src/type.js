5b9e9a085706ec0db9b02e5098bfa0a04d44fbd5
Bram Stein <b.l.stein@gmail.com>
2016-06-16 14:49:04 +0200

    Use buffer.toString instead of manually extracting tags.

--- src/type.js
+++ src/type.js
@@ -91,12 +91,7 @@ var Type = {
      * @return {string}
      */
     read: function (buffer, opt_byteOffset) {
-      var uint = buffer.readUInt32BE(opt_byteOffset || 0);
-
-      return String.fromCharCode((uint & 0xFF000000) >> 24) +
-             String.fromCharCode((uint & 0x00FF0000) >> 16) +
-             String.fromCharCode((uint & 0x0000FF00) >> 8) +
-             String.fromCharCode((uint & 0x000000FF) >> 0);
+      return buffer.toString('ascii', opt_byteOffset || 0, (opt_byteOffset || 0) + 4);
     }
   },
 
@@ -136,6 +131,40 @@ var Type = {
      */
     read: function (buffer, opt_byteOffset) {
       return buffer.readUIntBE(opt_byteOffset || 0, 3);
+    }
+  },
+
+  BASE128: {
+    /**
+     * Variable size, set automatically by the
+     * read method.
+     */
+    sizeof: 0,
+
+    /**
+     * @param {Buffer} buffer
+     * @param {number=} opt_byteOffset
+     * @return {number}
+     */
+    read: function (buffer, opt_byteOffset) {
+      var result = 0;
+      this.sizeof = 0;
+
+      for (var i = 0; i < 5; i++) {
+        data = buffer.readUInt8((opt_byteOffset || 0) + i);
+
+        if (result & 0xFE0000000) {
+          throw new Error('Base 128 number overflow');
+        }
+
+        result = (result << 7) | (data & 0x7F);
+
+        if ((data & 0x80) === 0) {
+          this.sizeof = i + 1;
+          return result;
+        }
+      }
+      throw new Error('Bad base 128 number');
     }
   }
 };
