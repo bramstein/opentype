fa452837ddf2d9d2ca26a16062161972b44dcc95
Bram Stein <b.l.stein@gmail.com>
2016-06-26 14:04:51 +0200

    Add more tests to ReadBuffer.

--- test/readbuffer-test.js
+++ test/readbuffer-test.js
@@ -1,33 +1,81 @@
 var ReadBuffer = require('../src/readbuffer');
 var Type = require('../src/type');
+var util = require('../src/util');
 var c = require('concentrate');
 var expect = require('unexpected');
 
 describe('ReadBuffer', function () {
-  it('creates a new buffer', function () {
-    var data = c()
-      .uint32be(4)
-      .result();
+  var data = null;
 
-    expect(new ReadBuffer(data), 'to be an object');
+  beforeEach(function () {
+    data = c()
+      .uint8(10)
+      .uint8(20)
+      .uint8(30)
+      .uint8(40)
+      .result();
   });
 
-  it('can read data', function () {
-    var data = c()
-      .uint32be(4)
-      .result();
+  it('creates a new ReadBuffer', function () {
+    var buffer = new ReadBuffer(data);
 
-    expect(new ReadBuffer(data).read(Type.ULONG), 'to equal', 4);
+    expect(buffer, 'to be an object');
+    expect(buffer.byteOffset, 'to equal', 0);
   });
 
-  it('can can read an array', function () {
-    var data = c()
-      .uint32be(10)
-      .uint32be(20)
-      .uint32be(30)
-      .result();
+  it('creates a new ReadBuffer with an offset', function () {
+    var buffer = new ReadBuffer(data, 2);
 
-    expect(new ReadBuffer(data).readArray(Type.ULONG, 3), 'to equal', [10, 20, 30]);
+    expect(buffer, 'to be an object');
+    expect(buffer.byteOffset, 'to equal', 2);
+  });
+
+  it('can goto any offset', function () {
+    var buffer = new ReadBuffer(data);
+
+    buffer.goto(2);
+    expect(buffer.byteOffset, 'to equal', 2);
+  });
+
+  it('can read bytes', function () {
+    var buffer = new ReadBuffer(data);
+
+    expect(buffer.read(Type.BYTE), 'to equal', 10);
+    expect(buffer.read(Type.BYTE), 'to equal', 20);
+    expect(buffer.read(Type.BYTE), 'to equal', 30);
+    expect(buffer.read(Type.BYTE), 'to equal', 40);
+  });
+
+  it('can goto any offset and read bytes', function () {
+    var buffer = new ReadBuffer(data);
+
+    buffer.goto(2);
+    expect(buffer.read(Type.BYTE, 2), 'to equal', 30);
+    expect(buffer.read(Type.BYTE, 1), 'to equal', 20);
+  });
+
+  it('can read an array of bytes', function () {
+    var buffer = new ReadBuffer(data);
+
+    expect(buffer.readArray(Type.BYTE, 4), 'to equal', [10, 20, 30, 40]);
+  });
+
+  it('can goto any offset and read an array of bytes', function () {
+    var buffer = new ReadBuffer(data);
+
+    expect(buffer.readArray(Type.BYTE, 2, 2), 'to equal', [30, 40]);
+  });
+
+  it('can read a complex struct', function () {
+    var buffer = new ReadBuffer(data);
+
+    expect(buffer.read(util.struct({ first: Type.BYTE, second: Type.BYTE })), 'to equal', { first: 10, second: 20 });
+  });
+
+  it('can read an array of complex structs', function () {
+    var buffer = new ReadBuffer(data);
+
+    expect(buffer.readArray(util.struct({ first: Type.BYTE, second: Type.BYTE }), 2), 'to equal', [{ first: 10, second: 20 }, { first: 30, second: 40 }]);
   });
 
   it('can read a string', function () {
@@ -36,5 +84,14 @@ describe('ReadBuffer', function () {
       .result();
 
     expect(new ReadBuffer(data).readString('ascii', 5), 'to equal', 'hello');
+  });
+
+  it('can read a string at an offset', function () {
+    var data = c()
+      .uint8(10)
+      .string('hello')
+      .result();
+
+    expect(new ReadBuffer(data).readString('ascii', 5, 1), 'to equal', 'hello');
   });
 });
